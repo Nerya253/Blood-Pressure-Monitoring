02295e63e6a3427d84f5901e776b36fdd8b0b714
Nerya253 <nerya253@gmail.com>
2025-02-22 17:14:12 +0200

    create CRUD for user

---
+++ Middleware/user_M.js
@@ -0,0 +1,80 @@
+const db_pool = require("../database");
+
+async function createUser(req, res, next) {
+    const full_name = req.body["new-user-name"];
+
+    try {
+        const promisePool = db_pool.promise();
+        const sqlQuery1 = `INSERT INTO users (full_name)
+                           VALUES (?)`;
+        const [result] = await promisePool.query(sqlQuery1, [full_name]);
+        req.lastInsertId = result.lastInsertId;
+        req.success = true;
+    } catch (error) {
+        console.error("Error in createUser:", error);
+        req.success = false;
+    }
+    next();
+}
+
+async function getUsers(req, res, next) {
+    const sqlQuery = `SELECT *
+                      FROM users`;
+    let rows = [];
+
+    try {
+        const promisePool = db_pool.promise();
+        [rows] = await promisePool.query(sqlQuery);
+        req.success = true;
+        req.users = rows;
+    } catch (err) {
+        req.success = false;
+        console.log(err);
+    }
+
+    next()
+}
+
+async function updateUser(req, res, next) {
+    let id = req.body.id;
+    const newName = req.body.full_name;
+
+    let sqlQuery = `UPDATE users `
+    sqlQuery += `SET full_name = '${newName}' `
+    sqlQuery += `WHERE id = ${id}`;
+    let rows = [];
+    try {
+        const promisePool = db_pool.promise();
+        [rows] = await promisePool.query(sqlQuery);
+        req.success = true;
+    } catch (err) {
+        req.success = false;
+        console.log(err);
+    }
+    next()
+}
+
+async function deleteUser(req, res, next) {
+    let id = req.body.id;
+    const sqlQuery = `DELETE
+                      FROM users
+                      WHERE id = ${id}`;
+    let rows = [];
+    try {
+        const promisePool = db_pool.promise();
+        [rows] = await promisePool.query(sqlQuery);
+        req.success = true;
+    } catch (err) {
+        req.success = false;
+        console.log(err);
+    }
+    next()
+}
+
+
+module.exports = {
+    createUser,
+    getUsers,
+    updateUser,
+    deleteUser
+}
