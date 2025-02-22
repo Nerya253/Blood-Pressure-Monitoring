d35e980450666ad53be9ccb5e4fe3cd8cba01dc2
Nerya253 <nerya253@gmail.com>
2025-02-22 17:07:24 +0200

    user router create four routers for the CRUD

---
+++ Routers/user_R.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const UserRouter = express.Router();
+
+module.exports = UserRouter;
+const user_M = require("../Middleware/user_M")
+
+UserRouter.post("/createUser", user_M.createUser, (req, res) => {//Create - הוספה
+    if (req.success) {
+        res.status(200).json({success: req.success, lastInsertId: req.lastInsertId})
+    } else {
+        res.status(500).send({success: req.success})
+    }
+})
+
+UserRouter.get("/getUsers", user_M.getUsers, (req, res) => {//Read - קבלת רשימה
+    if (req.success) {
+        res.status(200).send({success: req.success, students: req.students})
+    } else {
+        res.status(500).send({success: req.success})
+    }
+})
+
+UserRouter.put("/updateUser", user_M.updateUser, (req, res) => {//Update - עריכה
+    if (req.success) {
+        res.status(200).send({success: req.success})
+    } else {
+        res.status(500).send({success: req.success})
+    }
+})
+
+UserRouter.delete("/deleteUser", user_M.deleteUser, (req, res) => {// Delete - מחיקה
+    if (req.success) {
+        res.status(200).send({success: req.success})
+    } else {
+        res.status(500).send({success: req.success})
+    }
+})
